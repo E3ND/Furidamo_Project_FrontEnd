ecad1cbeb7b55066658a717ffa44ad7784d54371
E3ND <brunopimentelcityhotmail.com@gmail.com>
2025-06-17 15:02:22 -0300

    ✨ (Post.tsx): Add pagination functionality with left and right arrow icons for navigating through pages 📝 (styles.module.scss): Add styles for pagination component including arrow icons and page numbers styling

--- src/components/post/Post.tsx
+++ src/components/post/Post.tsx
@@ -1,6 +1,8 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faComment } from "@fortawesome/free-solid-svg-icons";
 import { faChevronUp } from "@fortawesome/free-solid-svg-icons";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 import Style from './styles.module.scss';
 import { Link } from "react-router-dom";
@@ -13,6 +15,21 @@ export default function Post() {
     ];
 
     return (
+        <div>
+
+        <div className={Style.filter}>
+            <select name="teste" id="teste2">
+                <option value="a">Mais recentes</option>
+                <option value="b">Mais antigo</option>
+                <option value="c">Mais curtido</option>
+                <option value="d">Mais comentados</option>
+            </select>
+
+            <div>
+                <input type="search" placeholder="Pesquisar" name="" id="" />
+            </div>
+        </div>
+
         <div className={Style.post_box}>
             <div className={Style.box_1}>
                 <p>7 <FontAwesomeIcon icon={faChevronUp} /> </p>
@@ -39,6 +56,28 @@ export default function Post() {
                     </div>
                 </div>
             </div>
+        </div>
+
+        <div className={Style.pagination}>
+            <div className={Style.pagination_box}>
+                <div className={Style.arrow_left}><FontAwesomeIcon icon={faArrowLeft} /></div>
+
+                <div className={Style.pagination_numbers}>
+                    <span className={Style.span}>1</span>
+                    <span className={Style.span}>2</span>
+                    <span className={Style.span_selected}>3</span>
+                    <span className={Style.span}>4</span>
+                    <span className={Style.span}>5</span>
+                    <span className={Style.span}>6</span>
+                    <span className={Style.span}>7</span>
+                    <span className={Style.span}>8</span>
+                    <span className={Style.span}>9</span>
+                    <span className={Style.span}>10</span>
+                </div>
+                <div className={Style.arrow_right}><FontAwesomeIcon icon={faArrowRight} /></div>
+            </div>
+        </div>
+
         </div>
     )
 }
