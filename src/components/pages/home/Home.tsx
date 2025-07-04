0e5debf289f1135378acfebde047422c586d95bc
E3ND <brunopimentelcityhotmail.com@gmail.com>
2025-07-04 14:54:05 -0300

    📝 (comment/styles.module.scss): Update color value for hover effect on reply_icon to use CSS variable --color-text for consistency and easier theming 📝 (pages/home/Home.tsx): Fix formatting by removing unnecessary spaces in useState declaration and aligning code blocks for better readability.
    📝 (home/styles.module.scss): Update color values to CSS custom properties for better maintainability and consistency
    📝 (postPage/styles.module.scss): Update color values to CSS custom properties for better maintainability and consistency
    
    ✨ (Profile.tsx): Introduce state management for menu items in Profile component to handle active menu item highlighting
    📝 (Profile.tsx): Add comments to clarify the purpose of certain code blocks in Profile component
    🔧 (Profile.tsx): Update styles in Profile component to improve layout and readability
    🔧 (styles.module.scss): Update styles in Profile component to use CSS variables for colors and improve consistency
    🔧 (styles.module.scss): Update styles in Post component to use CSS variables for colors and improve consistency

--- src/components/pages/home/Home.tsx
+++ src/components/pages/home/Home.tsx
@@ -21,7 +21,7 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Home() {
-    const [ disabledMenu, setDisabledMenu ] = useState(false);
+    const [disabledMenu, setDisabledMenu] = useState(false);
 
     const handleClickMenu = () => {
         setDisabledMenu(!disabledMenu);
@@ -110,39 +110,39 @@ export default function Home() {
                 </div>
             </div>
             <div className={Style.box_post}>
-                        <div className={Style.filter}>
-            <select name="teste" id="teste2">
-                <option value="a">Mais recentes</option>
-                <option value="b">Mais antigo</option>
-                <option value="c">Mais curtido</option>
-                <option value="d">Mais comentados</option>
-            </select>
+                <div className={Style.filter}>
+                    <select name="teste" id="teste2">
+                        <option value="a">Mais recentes</option>
+                        <option value="b">Mais antigo</option>
+                        <option value="c">Mais curtido</option>
+                        <option value="d">Mais comentados</option>
+                    </select>
 
-            <div>
-                <input type="search" placeholder="Pesquisar" name="" id="" />
-            </div>
-        </div>
+                    <div>
+                        <input type="search" placeholder="Pesquisar" name="" id="" />
+                    </div>
+                </div>
                 <Post />
 
-                        <div className={Style.pagination}>
-            <div className={Style.pagination_box}>
-                <div className={Style.arrow_left}><FontAwesomeIcon icon={faArrowLeft} /></div>
+                <div className={Style.pagination}>
+                    <div className={Style.pagination_box}>
+                        <div className={Style.arrow_left}><FontAwesomeIcon icon={faArrowLeft} /></div>
 
-                <div className={Style.pagination_numbers}>
-                    <span className={Style.span}>1</span>
-                    <span className={Style.span}>2</span>
-                    <span className={Style.span_selected}>3</span>
-                    <span className={Style.span}>4</span>
-                    <span className={Style.span}>5</span>
-                    <span className={Style.span}>6</span>
-                    <span className={Style.span}>7</span>
-                    <span className={Style.span}>8</span>
-                    <span className={Style.span}>9</span>
-                    <span className={Style.span}>10</span>
+                        <div className={Style.pagination_numbers}>
+                            <span className={Style.span}>1</span>
+                            <span className={Style.span}>2</span>
+                            <span className={Style.span_selected}>3</span>
+                            <span className={Style.span}>4</span>
+                            <span className={Style.span}>5</span>
+                            <span className={Style.span}>6</span>
+                            <span className={Style.span}>7</span>
+                            <span className={Style.span}>8</span>
+                            <span className={Style.span}>9</span>
+                            <span className={Style.span}>10</span>
+                        </div>
+                        <div className={Style.arrow_right}><FontAwesomeIcon icon={faArrowRight} /></div>
+                    </div>
                 </div>
-                <div className={Style.arrow_right}><FontAwesomeIcon icon={faArrowRight} /></div>
-            </div>
-        </div>
             </div>
         </div>
     )
--- src/components/pages/profile/Profile.tsx
+++ src/components/pages/profile/Profile.tsx
@@ -1,8 +1,28 @@
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+
 import Post from '../../post/Post';
 import Style from './styles.module.scss';
 
 export default function Profile() {
-    return(
+    const [arrayMenu, setArrayMenu] = useState(["list_type", "list_type_hover", "list_type"]);
+    let fakeArrayMenu = ["list_type", "list_type_hover", "list_type"];
+
+    const handleClickMyMenu = (id: number) => {
+        for (let i = 0; i < fakeArrayMenu.length; i++) {
+            if (i === id) {
+                fakeArrayMenu[i] = "list_type_hover";
+            } else {
+                fakeArrayMenu[i] = "list_type";
+            }
+        }
+
+        setArrayMenu(fakeArrayMenu);
+    }
+
+    return (
         <div className={Style.profile_container}>
             <div className={Style.profile_box}>
                 <div className={Style.banner}>
@@ -16,20 +36,26 @@ export default function Profile() {
                     <p>@E3ND</p>
                 </div>
                 <div className={Style.list_profile}>
-                    <div className={Style.list_type_hover}>
-                        <p>Meu posts</p>
-                    </div>
+                    {/* list_type_hover */}
+                    <div className={Style.profile_border}>
+                        <div className={Style[arrayMenu[0]]}>
+                            <p onClick={() => handleClickMyMenu(0)}>Likes</p>
+                        </div>
 
-                    <div className={Style.list_type}>
-                        <p>Likes</p>
-                    </div>
+                        <div className={Style[arrayMenu[1]]}>
+                            <p onClick={() => handleClickMyMenu(1)}>Minha conta</p>
+                        </div>
 
-                    <div className={Style.list_type}>
-                        <p>Amigos</p>
+                        <div className={Style[arrayMenu[2]]}>
+                            <p onClick={() => handleClickMyMenu(2)}>Amigos</p>
+                        </div>
                     </div>
                 </div>
 
-                <h4>Informações da conta</h4>
+                <div className={Style.profile_information_title}>
+                    <h4>Informações da conta</h4>
+                </div>
+
                 <div className={Style.profile_information_container}>
                     <div className={Style.profile_information_flex_1}>
                         <p>Nome</p>
@@ -40,7 +66,7 @@ export default function Profile() {
                     <div className={Style.profile_information_flex_2}>
                         <p>Nome Teste</p>
                         <p>@Nome aaa</p>
-                        <p>nome@gmail.com</p>
+                        <p>nomenome@gmail.com</p>
                         <div className={Style.profile_social}>
                             <p>@atatsa</p>
                             <p>@atatsa</p>
@@ -48,10 +74,32 @@ export default function Profile() {
                     </div>
                 </div>
 
-                <h4>Meus posts</h4>
-                <br /> <br /> <br />
+                <div className={Style.profile_information_title}>
+                    <h4>Meus posts</h4>
+                </div>
+
                 <div className={Style.box_post}>
-                    <Post />
+                    <div className={Style.box_post_component}><Post /></div>
+                </div>
+
+                <div className={Style.pagination}>
+                    <div className={Style.pagination_box}>
+                        <div className={Style.arrow_left}><FontAwesomeIcon icon={faArrowLeft} /></div>
+
+                        <div className={Style.pagination_numbers}>
+                            <span className={Style.span}>1</span>
+                            <span className={Style.span}>2</span>
+                            <span className={Style.span_selected}>3</span>
+                            <span className={Style.span}>4</span>
+                            <span className={Style.span}>5</span>
+                            <span className={Style.span}>6</span>
+                            <span className={Style.span}>7</span>
+                            <span className={Style.span}>8</span>
+                            <span className={Style.span}>9</span>
+                            <span className={Style.span}>10</span>
+                        </div>
+                        <div className={Style.arrow_right}><FontAwesomeIcon icon={faArrowRight} /></div>
+                    </div>
                 </div>
             </div>
         </div>
